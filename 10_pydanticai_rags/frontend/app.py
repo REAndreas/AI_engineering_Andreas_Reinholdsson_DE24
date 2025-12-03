1ad7f0dd8415b91a7cd42b62cb54a757a1cef314
Andreas <andreas.reinhol@gmail.com>
2025-12-03 15:04:04 +0100

    done video 10

---
+++ 10_pydanticai_rags/frontend/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import requests
+from pathlib import Path
+
+ASSETS_PATH = Path(__file__).absolute().parents[1] / "assets"
+
+def layout():
+    st.markdown("# Ragbit")
+    st.markdown("As a question about different dwarf rabbits")
+    text_input = st.text_input(label= "Ask a question")
+
+    if st.button("Send") and text_input != "":
+        response = requests.post("http://127.0.0.1:8000/rag/query", json={"prompt": text_input})
+
+        data = response.json()
+
+        st.markdown("## Question")
+        st.markdown(text_input)
+
+        st.markdown("## Answer")
+        st.markdown(data["answer"])
+
+        st.markdown("## Source")
+        st.markdown(data["filepath"])
+
+        st.image(ASSETS_PATH / f"{data["filename"]}.png")
+
+if __name__ == "__main__":
+    layout()
